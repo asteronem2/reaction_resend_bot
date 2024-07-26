bbebb838937c8aa85f0de369daa72543570292b5
asteronem2 <153909839+asteronem2@users.noreply.github.com>
2024-07-26 16:32:38 +0300

    ADMIN_LIST update

--- utils.py
+++ utils.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 
 class DotEnvData:
     BOT_TOKEN = None
-    OWNER_LIST = None
+    ADMIN_LIST = None
 
     def __init__(self):
         load_dotenv('param.env')
         environ = os.environ
         self.BOT_TOKEN = environ.get('BOT_TOKEN')
-        self.ADMIN_LISt = environ.get('ADMIN_LIST').split(' ')
+        self.ADMIN_LIST = environ.get('ADMIN_LIST').split(' ')
 
 
